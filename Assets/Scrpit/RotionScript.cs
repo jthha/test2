47915e68b415fa90b2affd1a29c775c243e76511
jthha <0wjsxoghks@gmail.com>
2015-03-23 18:30:27 +0900

    Revert "2015.03.23"
    This reverts commit 0945ad4ae7a1d11eaa5a2e494c00c2046e9e8677.

--- Assets/Scrpit/RotionScript.cs
+++ Assets/Scrpit/RotionScript.cs
@@ -1,31 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class RotionScript : MonoBehaviour {
-	public Vector3 _v;
-	public Text _text;
-	//private Gyroscope gyro;
+
 	// Use this for initialization
-
 	void Start () {
-
-		//gyro=Input.gyro;
-
-		//Debug.Log (gyro);
-		//Debug.Log (gyro.enabled);
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Debug.Log (gyro.rotationRateUnbiased.x);
-		_v=Input.acceleration*Mathf.Rad2Deg;
-		if (_v.z > 0) {
-			_text.text="뒤";
-		} else {
-			_text.text="앞";
-		}
-			
+	
 	}
 }
